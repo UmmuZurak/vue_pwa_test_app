9320c8c1009b094e9108fd15a46d33bd7d571d93
UmmuZurak <zurakummu@gmail.com>
2021-11-19 01:47:01 +0000

    made changes app.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="landing-page sidebar-collapse">
+    <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+      <div class="container">
+        <div class="navbar-translate">
+          <a class="navbar-brand" href="#">
+            <img src="../assets/download.png" alt="" style="width: 200px" />
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link"> Home </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link"> About </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link"> FAQS </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url('../assets/img/background.jpg')">
+      <div class="container">
+        <div v-if="deferredPrompt" class="alert alert-info margin-top w-100 mr-3 rounded desktop">
+          <div class="container">
+            <div class="alert-icon">
+              <i class="material-icons">info_outline</i>
+            </div>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true"><i class="material-icons">clear</i></span>
+            </button>
+            <b>Info alert:</b> You can install the Jetstream app by clicking on the install button. It won't take up space on your phone and also works offline!
+            <div class="d-flex justify-content-end mt-3">
+              <b class="mr-4 hover">INSTALL</b>
+              <b class="mr-5 hover" data-dismiss="alert" aria-label="Close">DISMISS</b>
+            </div>
+            <!-- <div class="d-flex justify-content-center mt-3"><button @click="install" class="btn btn-light mr-3">INSTALL</button> <button class="btn btn-danger" type="button" data-dismiss="alert" aria-label="Close">DISMISS</button></div> -->
+          </div>
+        </div>
+        <div v-if="deferredPrompt" class="alert alert-info w-100 mr-3 mt-5 rounded mobile">
+          <div class="container">
+            <div class="alert-icon">
+              <i class="material-icons">info_outline</i>
+            </div>
+            <!-- <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true"><i class="material-icons">clear</i></span>
+            </button> -->
+            <b>Info alert:</b> You can install the Jetstream app by clicking on the install button. It won't take up space on your phone and also works offline!
+            <div class="d-flex justify-content-end mt-3">
+              <b class="mr-4 hover">INSTALL</b>
+              <b class="mr-5 hover" data-dismiss="alert" aria-label="Close">DISMISS</b>
+            </div>
+            <!-- <div class="d-flex justify-content-center mt-3"><button @click="install" class="btn btn-light mr-3">INSTALL</button> <button class="btn btn-danger" type="button" data-dismiss="alert" aria-label="Close">DISMISS</button></div> -->
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="title">About Jetstream</h1>
+            <h4>Jetstream is a technology enabled logistics company. We aim to give businesses in emerging markets more control over their global supply chains.</h4>
+            <br />
+            <!-- <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-danger btn-raised btn-lg"> <i class="fa fa-play"></i> Watch video </a> -->
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -99,6 +81,29 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      deferredPrompt: null,
+    };
+  },
+  created() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+    window.addEventListener("appinstalled", () => {
+      this.deferredPrompt = null;
+    });
+  },
+  methods: {
+    async dismiss() {
+      this.deferredPrompt = null;
+    },
+    async install() {
+      this.deferredPrompt.prompt();
+    },
   },
 };
 </script>
@@ -118,5 +123,37 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.desktop {
+  display: block;
+}
+
+.mobile {
+  display: none;
+}
+
+.margin-top {
+  /* margin-top: -300px; */
+  position: absolute;
+  bottom: 300px;
+}
+
+.hover {
+  cursor: pointer;
+}
+
+@media only screen and (max-width: 767px) {
+  .desktop {
+    display: none;
+  }
+
+  .mobile {
+    display: block;
+  }
+
+  .title {
+    font-size: 30px;
+  }
 }
 </style>
